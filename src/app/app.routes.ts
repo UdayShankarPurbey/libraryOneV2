74b6d3d86d2e4d5616b5c20c34dd7f96a76b6fa0
udayshankar754 <uday.naxtre@gmail.com>
2024-05-28 15:19:48 +0530

    added Layout Pages

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { LayoutComponent } from './component/layout/layout.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full'
+  },
+  {
+    path : 'home',
+    component : LayoutComponent
+  }
+];
